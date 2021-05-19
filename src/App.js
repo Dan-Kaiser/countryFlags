48cb92b9a20a8adb5029b46c99e8da7a17f99f15
Dan Kaiser <Tophatman100@gmail.com>
2021-05-18 22:05:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+    return (<div>
+        <h1>This should be teal</h1>
+        <h2>Welcome to the React App boilerplate</h2>
+        <h3>Date : {new Date().toDateString()}</h3>
+    </div>)
+}
+
+export default App
