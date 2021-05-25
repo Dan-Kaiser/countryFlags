26cba93bc6e079d4907584fd716111a83b0fbfb2
Dan Kaiser <Tophatman100@gmail.com>
2021-05-24 20:28:01 -0500

    refactor how country codes are loaded, add hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FlagCard from "./components/FlagCard";
-import countries from "./assets/countries.json";
+import countriesJSON from "./assets/countryCodes.json";
 
 const App = () => {
+  const [currentCountryName, setCurrentCountryName] = useState("United States");
+  // const [currentCountryCode, setCurrentCountryCode] = useState("us");
+  const [flagImage, setFlagImage] = useState(`https://flagcdn.com/h240/us.png`);
+
+  const countries = Object.keys(countriesJSON);
+
+  useEffect(() => {
+    newRandomCountry();
+  }, []);
+
   const randomIntFromInterval = (min, max) => {
     // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-
-  const randomCountry =
-    countries[randomIntFromInterval(0, countries.length - 1)];
-  console.log(randomCountry);
-  const countryCode = randomCountry.countryCode.toLowerCase();
-  const { name } = randomCountry;
-  console.log(countryCode, name);
+  const newRandomCountry = () => {
+    const randomCountry =
+      countries[randomIntFromInterval(0, countries.length - 1)];
+    // setCurrentCountryCode(randomCountry);
+    setCurrentCountryName(countriesJSON[randomCountry]);
+    setFlagImage(`https://flagcdn.com/h240/${randomCountry}.png`);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("HandleSubmit Function Called");
+  };
 
   return (
     <div>
-      <h1>This should be teal</h1>
-      <h2>Welcome to the React App boilerplate</h2>
-      <h3>Date : {new Date().toDateString()}</h3>
       <FlagCard
-        flagImg={`https://flagcdn.com/h240/${countryCode}.png`}
-        // flagImg={`https://www.countryflags.io/${countryCode}/flat/64.png`}
-        countryName={name}
+        flagImg={flagImage}
+        countryName={currentCountryName}
+        handleSubmit={handleSubmit}
       />
     </div>
   );
--- src/components/FlagCard.js
+++ src/components/FlagCard.js
@@ -1,10 +1,31 @@
 import React from "react";
 
-const FlagCard = ({ flagImg, countryName }) => {
+const FlagCard = ({
+  flagImg,
+  countryName,
+  answerField,
+  updateAnswerField,
+  handleSubmit,
+}) => {
+  console.log("FlagCard Component Loaded");
+
   return (
     <div>
-      {countryName}
-      <img src={flagImg} />
+      <img src={flagImg} alt={`The flag of ${countryName}`} />
+      <p>{countryName}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Answer
+          <input
+            type="text"
+            name="answer"
+            id="answer"
+            // value={answerField}
+            // onChange={updateAnswerField}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
